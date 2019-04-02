6bf20ffb157bf518e41172dd5a042ad2c355af31
김민훈 <kmh357@tong.com>
2019-04-02 11:29:57 +0900

    error fix

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let url;
 if (process.env.NODE_ENV === "DEV") {
   url = "http://localhost:8081";
 } else {
-  url = "file://${process.cwd()}/dist/index.html";
+  url = `file://${process.cwd()}/dist/index.html`;
 }
 
 app.on("ready", () => {
